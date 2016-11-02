d1d250aa5e77880dd1e723f6b681fe0bd55a9608
imcczy <imcczyh@gmail.com>
2016-11-02 14:42:34 +0800

    format3

--- android_crack/tengxun2016/Tencent2016c_Keygen/app/src/main/java/com/imcczy/tencent2016c_keygen/MainActivity.java
+++ android_crack/tengxun2016/Tencent2016c_Keygen/app/src/main/java/com/imcczy/tencent2016c_keygen/MainActivity.java
@@ -3,21 +3,15 @@ package com.imcczy.tencent2016c_keygen;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.util.Log;
 import android.widget.TextView;
 import android.provider.Settings.Secure;
 
-import net.glxn.qrgen.android.QRCode;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 
 
@@ -36,13 +30,14 @@ public class MainActivity extends AppCompatActivity {
         if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_READ_PHONE_STATE);
         } else {
-            android_id = Secure.getString(getBaseContext().getContentResolver(), Secure.ANDROID_ID);
+            android_id = Secure.getString(getBaseContext().getContentResolver(), Secure.ENABLED_ACCESSIBILITY_SERVICES);
         }
-        android_id = keygen.b(android_id);
-        android_id = String.format("%s-%s-%s-%s-%s", android_id.substring(0, 8), android_id.substring(8, 12), android_id.substring(12, 16),
-                android_id.substring(16, 20), android_id.substring(20, 32));
-        String lisence = keygen.decrypt_id(android_id);
-        tv.setText(lisence);
+        //android_id = keygen.b(android_id);
+        //android_id = String.format("%s-%s-%s-%s-%s", android_id.substring(0, 8), android_id.substring(8, 12), android_id.substring(12, 16),
+        //        android_id.substring(16, 20), android_id.substring(20, 32));
+        //String lisence = keygen.decrypt_id(android_id);
+        //tv.setText(android_id);
+        Log.i("accessibility",android_id);
 
     }
 
@@ -51,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_READ_PHONE_STATE)
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                android_id = Secure.getString(getBaseContext().getContentResolver(), Secure.ANDROID_ID);
+                android_id = Secure.getString(getBaseContext().getContentResolver(), Secure.ENABLED_ACCESSIBILITY_SERVICES);
     }
 
 }
